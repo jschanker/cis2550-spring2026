ef18d354ed96ec17c77fde29fcd3a584763f8ab1
jschanker <jschanker@molloy.edu>
2026-01-19 21:03:32 -0500

    fix: CORS for POST

--- index.js
+++ index.js
@@ -1,25 +1,30 @@
-const Hapi = require('@hapi/hapi');
-const path = require('path');
-const fs = require('fs');
+const Hapi = require("@hapi/hapi");
+const path = require("path");
+const fs = require("fs");
 
 const init = async () => {
-  const server = Hapi.server({});
+  const server = Hapi.server({
+    routes: {
+      cors: {
+        origin: ["*"],
+      },
+    },
+  });
 
   const routes = [];
-  const routesPath = path.join(__dirname, 'routes');
+  const routesPath = path.join(__dirname, "routes");
 
   fs.readdirSync(routesPath).forEach((fileName) =>
-    routes.push(...require(path.join(routesPath, fileName)))
+    routes.push(...require(path.join(routesPath, fileName))),
   );
 
   server.route(routes);
 
   await server.start();
-  console.log('Server running on %s', server.info.uri);
+  console.log("Server running on %s", server.info.uri);
 };
 
-process.on('unhandledRejection', (err) => {
-
+process.on("unhandledRejection", (err) => {
   console.log(err);
   process.exit(1);
 });
