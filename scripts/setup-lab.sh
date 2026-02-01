d1f7a9c445465a90c3b1e7a82d44650107662130
jschanker <jschanker@molloy.edu>
2026-02-01 00:09:43 -0500

    See current working directory

--- scripts/setup-lab.sh
+++ scripts/setup-lab.sh
@@ -6,10 +6,14 @@ TARGET_DIR="$LAB_DIR/var/www/images/icons"
 rm -rf "$LAB_DIR"
 mkdir -p "$LAB_DIR"
 
+echo "--- Current Directory ---"
+ls -lt .
+echo "-------------------------------"
+
 mkdir -p "$LAB_DIR/etc"
 mkdir -p "$LAB_DIR/var/log"
 mkdir -p "$LAB_DIR/home/user"
-mkdir -p $TARGET_DIR
+mkdir -p "$TARGET_DIR"
 
 # 1. Document Icon
 cat << 'EOF' > "$TARGET_DIR/document.svg"
@@ -54,11 +58,11 @@ EOF
 mkdir -p public
 touch public/index.html
 
-echo "root:x:0:0:root:/root:/bin/bash" > $LAB_DIR/etc/passwd
-echo "games:x:5:60:games:/usr/games:/sbin/nologin" >> $LAB_DIR/etc/passwd
-echo "sbx_user105:x:990:990:Sandbox User:/:/sbin/nologin" >> $LAB_DIR/etc/passwd
+echo "root:x:0:0:root:/root:/bin/bash" > "$LAB_DIR/etc/passwd"
+echo "games:x:5:60:games:/usr/games:/sbin/nologin" >> "$LAB_DIR/etc/passwd"
+echo "sbx_user105:x:990:990:Sandbox User:/:/sbin/nologin" >> "$LAB_DIR/etc/passwd"
 
-echo "FLAG{TRAVERSAL_MASTER_2026}" > $LAB_DIR/var/log/system.log
-echo "Secret note: The admin password is hidden in the environment variables." > $LAB_DIR/home/user/note.txt
+echo "FLAG{TRAVERSAL_MASTER_2026}" > "$LAB_DIR/var/log/system.log"
+echo "Secret note: The admin password is hidden in the environment variables." > "$LAB_DIR/home/user/note.txt"
 
 echo "Lab environment provisioned successfully."
